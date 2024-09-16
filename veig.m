0ef1c64b3069d0c3dcb2ff9877cf5e7401fa7801
chibaf <chibaf@mac.com>
2024-09-16 20:55:23 +0900

    Add files via upload

--- veig.m
+++ veig.m
@@ -3,25 +3,23 @@ function veig
 	N=4;
 	A=[1 2 1 2;2 2 -1 1;1 -1 1 1;2 1 1 1]
 	[P,D]=eig(A)
-	%display("down:\n")
-	setround(-1)
+	setround(-1)  % round toward downward
 	V=P*D;
-	%display("up:\n")
-	setround(1)
+	setround(1)  % round toward upward
 	VV=P*D;
 	mV=V+0.5*(VV-V);
 	rV=mV-V;
-	%display("down:\n")
-	setround(-1)
+	setround(-1)  % round toward downward
 	E=mV*P'+(-rV)*abs(P')-A;
 	F=P*P'-eye(N);
-	%display("up:\n")
-	setround(1)
+	setround(1)  % round toward upward
 	EE=mV*P'+rV*abs(P')-A;
 	FF=P*P'-eye(N);
 	k=max(abs(E),abs(EE));
 	k2=max(abs(F),abs(FF));
 	e1=norm(k,inf)
 	e2=norm(k2,inf)
+	display("\n")
+	display(" errors of approximate eigenvaues\n")
 	e=diag(abs(D)*e2+e1*eye(N))
 endfunction
